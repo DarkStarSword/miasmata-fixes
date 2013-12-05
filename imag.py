03e248a595157d023aacc8dd39c1848902694a7b
Ian Munsie <darkstarsword@gmail.com>
2013-12-05 16:08:41 +1100

    imag.py: Optimise the hell out of it!
    Forget using a PIL file decoder plugin - that restricts me far too much
    to write a semi-fast implementation since I'm not writing it in C.
    
    Instead, use the multiprocessing module to construct image strips in
    parallel, then stitch them back together at the end. Also try to avoid
    rearranging data within each 4x4 block which was killing my performance
    before.
    
    With this, my dual-core i5 can load a 1024x1024 image in under a second.
    
    It's still not going to be anywhere near as fast as an optimal C
    implementation using SIMD type instructions, let alone a GPU
    implementation, but it will suffice for my purposes.

--- imag.py
+++ imag.py
@@ -3,193 +3,167 @@
 import sys
 import Image, ImageFile
 import struct
+import multiprocessing
 from StringIO import StringIO
 
-class DDSImageFile(ImageFile.ImageFile):
-	format = format_description = "DDS"
+# Documentation:
+# https://en.wikipedia.org/wiki/S3_Texture_Compression
+# http://msdn.microsoft.com/en-us/library/windows/desktop/bb943991(v=vs.85).aspx
 
-	# Documentation:
-	# https://en.wikipedia.org/wiki/S3_Texture_Compression
-	# http://msdn.microsoft.com/en-us/library/windows/desktop/bb943991(v=vs.85).aspx
+class DDSPixelFormat(object):
+	# http://msdn.microsoft.com/en-us/library/windows/desktop/bb943984(v=vs.85).aspx
+	class Flags(object):
+		ALPHAPIXELS = 0x00001
+		ALPHA       = 0x00002
+		FOURCC      = 0x00004
+		RGB         = 0x00040
+		YUV         = 0x00200
+		LUMINANCE   = 0x20000
 
-	class DDSPixelFormat(object):
-		# http://msdn.microsoft.com/en-us/library/windows/desktop/bb943984(v=vs.85).aspx
-		class Flags(object):
-			ALPHAPIXELS = 0x00001
-			ALPHA       = 0x00002
-			FOURCC      = 0x00004
-			RGB         = 0x00040
-			YUV         = 0x00200
-			LUMINANCE   = 0x20000
+	def __init__(self, fp):
+		(
+			size,
+			self.flags,
+			four_cc,
+			rgb_bit_count,
+			r_bit_mask,
+			g_bit_mask,
+			b_bit_mask,
+			a_bit_mask
+		) = struct.unpack('<2I 4s 5I', fp.read(32))
 
-		def __init__(self, fp):
-			(
-				size,
-				self.flags,
-				four_cc,
-				rgb_bit_count,
-				r_bit_mask,
-				g_bit_mask,
-				b_bit_mask,
-				a_bit_mask
-			) = struct.unpack('<2I 4s 5I', fp.read(32))
-
-			assert(size == 32)
-			if self.flags & self.Flags.ALPHAPIXELS: # uncompressed
-				self.a_bit_mask = a_bit_mask
-			assert(not self.flags & self.Flags.ALPHA) # old file
-			if self.flags & self.Flags.FOURCC:
-				self.four_cc = four_cc
-			if self.flags & self.Flags.RGB: # uncompressed
-				self.rgb_bit_count = rgb_bit_count
-				self.r_bit_mask = r_bit_mask
-				self.g_bit_mask = g_bit_mask
-				self.b_bit_mask = b_bit_mask
-			assert(not self.flags & self.Flags.YUV) # old file
-			assert(not self.flags & self.Flags.LUMINANCE) # old file
+		assert(size == 32)
+		if self.flags & self.Flags.ALPHAPIXELS: # uncompressed
+			self.a_bit_mask = a_bit_mask
+		assert(not self.flags & self.Flags.ALPHA) # old file
+		if self.flags & self.Flags.FOURCC:
+			self.four_cc = four_cc
+		if self.flags & self.Flags.RGB: # uncompressed
+			self.rgb_bit_count = rgb_bit_count
+			self.r_bit_mask = r_bit_mask
+			self.g_bit_mask = g_bit_mask
+			self.b_bit_mask = b_bit_mask
+		assert(not self.flags & self.Flags.YUV) # old file
+		assert(not self.flags & self.Flags.LUMINANCE) # old file
 
 
-	class DDSHeader(object):
-		# http://msdn.microsoft.com/en-us/library/windows/desktop/bb943982(v=vs.85).aspx
-		class Flags(object):
-			# Note: Don't rely on these flags - not all writers set them
-			CAPS        = 0x000001
-			HEIGHT      = 0x000002
-			WIDTH       = 0x000004
-			PITCH       = 0x000008
-			PIXELFORMAT = 0x001000
-			MIPMAPCOUNT = 0x020000
-			LINEARSIZE  = 0x080000
-			DEPTH       = 0x800000
-			REQUIRED = CAPS | HEIGHT | WIDTH | PIXELFORMAT
+class DDSHeader(object):
+	# http://msdn.microsoft.com/en-us/library/windows/desktop/bb943982(v=vs.85).aspx
+	class Flags(object):
+		# Note: Don't rely on these flags - not all writers set them
+		CAPS        = 0x000001
+		HEIGHT      = 0x000002
+		WIDTH       = 0x000004
+		PITCH       = 0x000008
+		PIXELFORMAT = 0x001000
+		MIPMAPCOUNT = 0x020000
+		LINEARSIZE  = 0x080000
+		DEPTH       = 0x800000
+		REQUIRED = CAPS | HEIGHT | WIDTH | PIXELFORMAT
 
-		def __init__(self, fp):
-			(
-				size,
-				self.flags,
-				self.height,
-				self.width,
-				self.pitch_or_linear_size,
-				self.depth,
-				self.mip_map_count
-			) = struct.unpack('<7I 44x', fp.read(72))
-			self.pixel_format = DDSImageFile.DDSPixelFormat(fp)
-			(
-				self.caps,
-				self.caps2,
-				self.caps3,
-				self.caps4
-			) = struct.unpack('<4I4x', fp.read(20))
-			assert(size==124)
-			assert(self.flags & self.Flags.REQUIRED == self.Flags.REQUIRED)
+	def __init__(self, fp):
+		(
+			size,
+			self.flags,
+			self.height,
+			self.width,
+			self.pitch_or_linear_size,
+			self.depth,
+			self.mip_map_count
+		) = struct.unpack('<7I 44x', fp.read(72))
+		self.pixel_format = DDSPixelFormat(fp)
+		(
+			self.caps,
+			self.caps2,
+			self.caps3,
+			self.caps4
+		) = struct.unpack('<4I4x', fp.read(20))
+		assert(size==124)
+		assert(self.flags & self.Flags.REQUIRED == self.Flags.REQUIRED)
 
-	def _open(self):
-		if self.fp.read(4) != 'DDS ':
-			raise SyntaxError('Not a DDS file')
-		self.header = DDSImageFile.DDSHeader(self.fp)
-		assert(self.fp.tell() == 128)
-		assert(self.header.pixel_format.four_cc) == 'DXT5'
+def dtx1(pix, x, y, c0, c1, clookup):
+	def rgb565(c):
+		return [(c & 0xf800) >> 8,
+			(c & 0x07e0) >> 3,
+			(c & 0x001f) << 3]
+	table = [ rgb565(c0), rgb565(c1) ]
+	z = zip(*table)
+	if c0 > c1:
+		table.append([ (c0 * 2 + c1) / 3 for (c0, c1) in z ])
+		table.append([ (c1 * 2 + c0) / 3 for (c0, c1) in z ])
+	else:
+		table.append([ (c0     + c1) / 2 for (c0, c1) in z ])
+		table.append([0, 0, 0]) # >Transparent< black? Alpha in DTX1?
 
-		self.size = (self.header.width, self.header.height)
+	for y1 in xrange(y, y+4):
+		for x1 in xrange(x, x+4):
+			l = clookup & 0x3
+			clookup >>= 2
+			pix[x1, y1] = tuple(table[l])
 
-		if self.header.pixel_format.four_cc == 'DXT1':
-			self.mode = 'RGB'
-		else:
-			self.mode = 'RGBA'
+def dtx5(pix, x, y, alpha):
+	# Byte swapped due to reading in LE
+	a0      = (alpha & 0x00000000000000ff) >> 0
+	a1      = (alpha & 0x000000000000ff00) >> 8
+	alookup = (alpha & 0xffffffffffff0000) >> 16
 
-		self.tile = []
-		for y in range(0, self.header.height, 4):
-			# for x in range(0, self.header.width, 4):
-			# 	self.tile.append((
-			# 		('raw', (x, y, x+4, y+4),
-			# 			0x80 + (y*self.header.width + x) / 4 * 128,
-			# 			(self.mode, 0, 1))
-			# 	))
-			self.tile.append((
-				('raw', (0, y, self.header.width, y+4),
-					0x80 + (y*self.header.width),
-					('RGBA', 0, 1))
-			))
-		print len(self.tile)
+	table = [a0, a1]
 
-	@staticmethod
-	def _dtx1(c0, c1, clookup):
-		def rgb565(c):
-			return [(c & 0xf800) >> 8,
-			        (c & 0x07e0) >> 3,
-			        (c & 0x001f) << 3]
-		rgbs = ( rgb565(c0), rgb565(c1) )
-		table = [ struct.pack('3B', *c) for c in rgbs ]
-		z = zip(*rgbs)
-		if c0 > c1:
-			table.append(struct.pack('3B', *[ (c0 * 2 + c1) / 3 for (c0, c1) in z ]))
-			table.append(struct.pack('3B', *[ (c1 * 2 + c0) / 3 for (c0, c1) in z ]))
-		else:
-			table.append(struct.pack('3B', *[ (c0     + c1) / 2 for (c0, c1) in z ]))
-			table.append('\0\0\0') # >Transparent< black? Alpha in DTX1?
+	if a0 > a1:
+		for i in xrange(1, 7):
+			table.append(((7-i)*a0 + i*a1) / 7)
+	else:
+		for i in xrange(1, 5):
+			table.append(((5-i)*a0 + i*a1) / 5)
+		table.append(0)
+		table.append(255)
 
-		ret = []
-		for y in range(4):
-			row = []
-			for x in range(4):
-				shift = (y * 4 + x) * 2
-				l = (clookup >> shift) & 0x3;
-				row.append(table[l])
-			ret.append(row)
-		return ret
+	for y1 in xrange(y, y+4):
+		for x1 in xrange(x, x+4):
+			l = alookup & 0x7
+			alookup >>= 3
+			pix[x1, y1] = pix[x1, y1][0:3] + (table[l],)
 
-	def dtx1(self):
-		return self._dtx1(*struct.unpack('<HHI'))
+def process_strip((y, mode, buf)):
+	width = len(buf) / 4
+	buf = StringIO(buf)
+	image = Image.new(mode, (width, 4))
+	pix = image.load()
+	for x in xrange(0, width, 4):
+		(alpha, c0, c1, clookup) = struct.unpack('<QHHI', buf.read(16))
+		dtx1(pix, x, 0, c0, c1, clookup)
+		if mode == 'RGBA':
+			dtx5(pix, x, 0, alpha)
+	return (y, image.tostring())
 
-	@staticmethod
-	def _dtx45_alpha(alpha):
-		# Byte swapped due to reading in LE
-		a0      = (alpha & 0x00000000000000ff) >> 0
-		a1      = (alpha & 0x000000000000ff00) >> 8
-		alookup = (alpha & 0xffffffffffff0000) >> 16
+def open_dds(fp, mipmap=None, mode='RGBA'):
+	if fp.read(4) != 'DDS ':
+		raise ValueError('Not a DDS file')
+	header = DDSHeader(fp)
+	assert(header.pixel_format.four_cc) == 'DXT5'
 
-		table = [chr(a0), chr(a1)]
+	(width, height) = (header.width, header.height)
+	if mipmap:
+		while mipmap < (width, height):
+			fp.seek(width * height, 1)
+			(width, height) = (width/2, height/2)
 
-		if a0 > a1:
-			for i in range(1, 7):
-				table.append(chr(((7-i)*a0 + i*a1) / 7))
-		else:
-			for i in range(1, 5):
-				table.append(chr(((5-i)*a0 + i*a1) / 5))
-			table.append('\0')
-			table.append('\xff')
+	buf = [ (y, mode, fp.read(16 * width / 4)) for y in xrange(0, height, 4) ]
 
-		ret = []
-		for y in range(4):
-			row = []
-			for x in range(4):
-				shift = (y * 4 + x) * 3
-				l = (alookup >> shift) & 0x7
-				row.append(table[l])
-			ret.append(row)
-		return ret
+	image = Image.new(mode, (width, height))
+	pix = image.load()
 
-	def dtx5(self):
-		(alpha, c0, c1, clookup) = struct.unpack('<QHHI', self.fp.read(16))
-		ret = self._dtx1(c0, c1, clookup)
-		aret = self._dtx45_alpha(alpha)
-		for y in range(4):
-			ret[y] = [ c+a for (c,a) in zip(ret[y], aret[y]) ]
-		return ret
+	pool = multiprocessing.Pool()
 
-	i=0
-	def load_read(self, size):
-		print>>sys.stderr, self.i
-		self.i+=1
-		ret = [[], [], [], []]
-		for x in range(0, self.header.width, 4):
-			r = self.dtx5()
-			for y in range(4):
-				ret[y].extend(r[y])
+	for (y, strip) in pool.imap_unordered(process_strip, buf, height / 16):
+		image.paste(Image.fromstring(mode, (width, 4), strip), (0, y))
 
-		return ''.join( [ ''.join(y) for y in ret ] )
+	return image
 
 
-
-Image.register_open('DDS', DDSImageFile)
-Image.register_extension('DDS', '.dds')
+if __name__ == '__main__':
+	f = open('main/TEX/Map_FilledIn.dds', 'rb')
+	# image = open_dds(f)
+	# image = open_dds(f, (2048, 2048))
+	image = open_dds(f, (1024, 1024), 'RGB')
+	image.save('test.jpg')
