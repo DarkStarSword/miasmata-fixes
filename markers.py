d274838deff0ebef980cbc2c293f268729f6113a
Ian Munsie <darkstarsword@gmail.com>
2014-01-21 16:18:34 +1100

    markers.py: Update code to use Rs5ChunkedFileDecoder

--- markers.py
+++ markers.py
@@ -4,35 +4,31 @@ import struct
 import sys
 
 import rs5file
-import miasmap
 
 def parse_nlst_header(f):
-	assert(f.read(10) == '\0\0NLST\0\0\1\0')
-	(chunk_size, u1, num_entries, u2) = struct.unpack('<H6sH2s', f.read(12))
-	assert(u1 == '\0'*6)
+	(num_entries, u2) = struct.unpack('<H2s', f.read(4))
 	assert(u2 == '\0'*2)
-	return (chunk_size, num_entries)
+	return num_entries
 
-def parse_mlst_header(f):
-	assert(f.read(10) == '\0\0MLST\0\0\1\0')
-	(chunk_size, u1) = struct.unpack('<H6s', f.read(8))
-	assert(u1 == '\0'*6)
-	return chunk_size
+def parse_markers(chunks):
+	nlst = chunks['NLST']
+	mlst = chunks['MLST']
 
-def parse_markers():
-	f = open('markers')
-	filesize = rs5file.parse_raw_header(f)
-	(chunk_size, num_entries) = parse_nlst_header(f)
-	nlst = f.read(chunk_size).rstrip('\0').split('\0')
-	chunk_size = parse_mlst_header(f)
-	for i in range(chunk_size/6/4):
+	f = nlst.get_fp()
+	num_entries = parse_nlst_header(f)
+	nlst = f.read(nlst.size - 4).rstrip('\0').split('\0')
+
+	f = mlst.get_fp()
+	for i in range(mlst.size / 6 / 4):
 		(idx, u1, x, y, z, u2) = struct.unpack('<2I4f', f.read(6*4))
 		yield (idx, nlst[idx], u1, x, y, z, u2)
 
 def main():
+	import miasmap
+	chunks = rs5file.Rs5ChunkedFileDecoder(open('markers').read())
 	filters = sys.argv[1:]
 	colours = ((128, 0, 0), (0, 128, 0), (0, 0, 128), (128, 128, 0), (128, 0, 128), (0, 128, 128), (128, 128, 128))
-	for (idx, name, u1, x, y, z, u2) in parse_markers():
+	for (idx, name, u1, x, y, z, u2) in parse_markers(chunks):
 		c = (128, 128, 128)
 		if filters:
 			for (i, f) in enumerate(filters):
