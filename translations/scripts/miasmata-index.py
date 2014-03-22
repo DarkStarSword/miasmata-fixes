175cc82aebdb8aeecabcf27d8bd0294e5447ccaf
Ian Munsie <darkstarsword@gmail.com>
2014-03-22 23:21:51 +1100

    Script for journal index titles

--- translations/scripts/miasmata-index.py
+++ translations/scripts/miasmata-index.py
@@ -3,38 +3,40 @@
 from gimpfu import *
 from miasmata_gimp import *
 
-font = 'Neu Phollick Alpha'
-line_spacing = -7.0
-letter_spacing = 0.4
+font_s = Font('Neu Phollick Alpha', 55.0, True, -7.0, letter_spacing=0.4)
+font_l = Font('Neu Phollick Alpha', 68.0, True, -7.0, letter_spacing=0.4)
+font_t = Font('Neu Phollick Alpha', 40.0, True)
 
 x = 205
-w = 1000 - x
+w = 995 - x
 
-def font_size(name):
+def font(name):
     if name == 'LIST_IAmCured' or name.upper().startswith('LIST_NOTE'):
-        return 68.0
-    return 55.0
+        return font_l
+    return font_s
 
 def compose_index_image(source_txt_file, source_blank_image, output_basename):
     txt = open(source_txt_file, 'rb').read().decode('utf-8').strip()
 
     image = pdb.gimp_file_load(source_blank_image, source_blank_image)
 
-    text = add_text_layer(image, txt, font, font_size(output_basename))
+    text = add_text(image, txt, font(output_basename))
     pdb.gimp_text_layer_set_justification(text, TEXT_JUSTIFY_CENTER)
-    pdb.gimp_text_layer_set_line_spacing(text, line_spacing)
-    pdb.gimp_text_layer_set_letter_spacing(text, letter_spacing)
     if output_basename in ('LIST_X', 'LIST_Y', 'LIST_Z', 'List_K'):
         pdb.gimp_layer_set_mode(text, MULTIPLY_MODE)
     else:
         pdb.gimp_layer_set_mode(text, BURN_MODE)
 
-    # bold_text(text, txt)
-    # pdb.gimp_text_layer_resize(text, w, text.height)
     bold_word_wrap(text, txt, w)
 
     pdb.gimp_layer_translate(text, x + (w - text.width) / 2, (image.height - text.height) / 2)
 
+    save(image, output_basename)
+
+def compose_index_title_image(source_txt_name, source_blank_image, output_basename):
+    image = pdb.gimp_file_load(source_blank_image, source_blank_image)
+    layer = add_text_layer_from_file(image, source_txt_name, font_t)
+    place_text(layer, 220, 10)
     save(image, output_basename)
 
 register(
@@ -53,6 +55,24 @@ register(
     ],
     [],
     compose_index_image,
+)
+
+register(
+    "miasmata_index_title",
+    "Compose a title image for Miasmata's Journal index pages",
+    "Compose a title image for Miasmata's Journal index pages",
+    "Ian Munsie",
+    "Ian Munsie",
+    "2014",
+    "<Toolbox>/_Miasmata/_Index",
+    None,
+    [
+        (PF_FILE, "source_txt_file", "utf-8 encoded file with the text to place on the image", None),
+        (PF_FILE, "source_blank_image", "Background image to use that should have previously had the text removed", None),
+        (PF_STRING, "output_basename", "Base output filename", None),
+    ],
+    [],
+    compose_index_title_image,
 )
 
 main()
--- translations/scripts/miasmata-index.sh
+++ translations/scripts/miasmata-index.sh
@@ -2,9 +2,17 @@
 
 [ -z "$GIMP" ] && GIMP=gimp
 
-for textfile in *.txt
+for basename in LIST_TITLE_Notes LIST_TITLE_Research
 do
-	basename=$(basename "$textfile" ".txt")
-	blank=$(dirname "$textfile")/../../blanks/index/${basename}_blank.png
+	textfile="$basename".txt
+	blank=../../blanks/index/LIST_TITLE_Placeholder.png
+	${GIMP} --no-interface --batch '(python-fu-miasmata-index-title RUN-NONINTERACTIVE "'${textfile}'" "'${blank}'" "'${basename}'")' --batch '(gimp-quit 1)'
+done
+
+for basename in LIST_00a LIST_00b LIST_0a LIST_0b LIST_10a LIST_10b LIST_11a LIST_11b LIST_12a LIST_12b LIST_13a LIST_13b LIST_14a LIST_14b LIST_15a LIST_15b LIST_16a LIST_16b LIST_17a LIST_17b LIST_18a LIST_18b LIST_19a LIST_19b LIST_1a LIST_1b LIST_20a LIST_20b LIST_21a LIST_21b LIST_22a LIST_22b LIST_23a LIST_23b LIST_24a LIST_24b LIST_25a LIST_25b LIST_26a LIST_26b LIST_27a LIST_27b LIST_28a LIST_28b LIST_29a LIST_29b LIST_2a LIST_2b LIST_30a LIST_30b LIST_31a LIST_31b LIST_32a LIST_32b LIST_33a LIST_33b LIST_34a LIST_34b LIST_3a LIST_3b LIST_4a LIST_4b LIST_5a LIST_5b LIST_6a LIST_6b LIST_7a LIST_7b LIST_8a LIST_8b LIST_9a LIST_9b LIST_A LIST_B LIST_C LIST_D LIST_E LIST_F LIST_H LIST_IAmCured LIST_I LIST_J LIST_NoResult LIST_Note0 LIST_Note10 LIST_Note11 LIST_Note12 LIST_Note13 LIST_Note14 LIST_Note15 LIST_Note16 LIST_Note17 LIST_Note18 LIST_Note1 LIST_Note2 LIST_Note3 LIST_Note4 LIST_Note5 LIST_Note7 LIST_Note8 LIST_Note9 LIST_NoteA1 LIST_NoteA2 LIST_NoteA3 LIST_NoteA4 LIST_NoteA5 LIST_NOTEA6 LIST_NoteA7 LIST_NoteA8 LIST_NoteA9 LIST_NoteAA LIST_NoteBB LIST_NoteCC LIST_NoteDD LIST_NoteEE LIST_NoteFF LIST_NoteGG LIST_NoteHH LIST_NoteII LIST_NoteJJ LIST_NoteKK LIST_NoteLL LIST_NoteMM LIST_NoteNN LIST_NoteOO LIST_NotePP LIST_NoteQQ LIST_NoteRR LIST_NoteSS LIST_NoteTT LIST_NoteUU LIST_NoteVV LIST_NoteWW LIST_NoteXX LIST_NoteYY LIST_NoteZZ LIST_X LIST_Y LIST_Z List_K
+do
+
+	textfile="$basename".txt
+	blank=../../blanks/index/${basename}_blank.png
 	${GIMP} --no-interface --batch '(python-fu-miasmata-index RUN-NONINTERACTIVE "'${textfile}'" "'${blank}'" "'${basename}'")' --batch '(gimp-quit 1)'
 done
