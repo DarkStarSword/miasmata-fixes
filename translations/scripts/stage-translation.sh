8c9fd4d49f33bd92ad9bd9f63305e0e72ad9147d
Ian Munsie <darkstarsword@gmail.com>
2014-04-06 03:53:08 +1000

    stage_translation: Add files from the community patch
    We want the translation to be an all in one patch which includes the
    most recent community patch.
    
    I might put some smarts into the installer to keep the community patch
    separate so Misamod can be a bit intelligent when updating mods - in
    which case I actually don't want to do this, but for now...

--- translations/scripts/stage-translation.sh
+++ translations/scripts/stage-translation.sh
@@ -3,6 +3,8 @@
 headers=$PWD/miasmata-headers
 translation=/var/www/miasmata/fr
 staging=$PWD/miasmata-fr
+miasmata_fixes_top=$(readlink -f $(dirname $(readlink -f "$0"))/../../)
+communitypatch=${miasmata_fixes_top}/communitypatch
 
 link_dds_files()
 {
@@ -14,12 +16,15 @@ link_dds_files()
 	for file in $(find "$src" -name "$pattern"); do
 		name=$(basename "$file" .dds)
 		mkdir -vp "$staging/$dst/$name"
-		ln -sv "$headers/$dst/$name/"* "$staging/$dst/$name/"
-		ln -sv "$file" "$staging/$dst/$name/02-DATA.dds"
+		ln -sfv "$headers/$dst/$name/"* "$staging/$dst/$name/"
+		ln -sfv "$file" "$staging/$dst/$name/02-DATA.dds"
 	done
 }
 
 rm -frv "$staging"
+
+mkdir -p "$staging"
+cp -rsv "$communitypatch"/main/* "$staging"
 
 mkdir -p "$staging/TEX/J2"
 for dir in index drugs plants research objectives conditions tabs; do
