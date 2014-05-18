c0d519e18787bfb8f5b263b548f3bd20abe606d6
Ian Munsie <darkstarsword@gmail.com>
2014-05-19 01:28:50 +1000

    Convert miaspatch French bbfreeze shell script into Windows batch file
    Force of habit...

---
+++ miaspatch-bbfreeze-fr.bat
@@ -0,0 +1,1 @@
+c:\Python27\python.exe miaspatch-bbfreeze.py -d miasmata-french --language fr_FR translations/french.miasmod translations/french.rs5mod
--- miaspatch-bbfreeze-fr.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-./miaspatch-bbfreeze.py -d miasmata-french --language fr_FR translations/french.miasmod translations/french.rs5mod
