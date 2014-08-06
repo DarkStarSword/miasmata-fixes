b1584d5ea4eb7e4c4d1d930c70064a4bea4d20cb
Ian Munsie <darkstarsword@gmail.com>
2014-08-07 03:06:03 +1000

    Miaschiev: Remove reminder from build script

--- miaschiev-bbfreeze.py
+++ miaschiev-bbfreeze.py
@@ -1,15 +1,8 @@
 #!/usr/bin/env python
 
-print 'To use gui_only with bbfreeze:'
-print '- Prevent console output - it will crash!'
-print
-print 'Press Enter...'
-raw_input()
-
 from bbfreeze import Freezer
 f = Freezer("miaschiev")
 f.addScript("miaschiev.pyw", gui_only=True)
-# f.addScript("miaschiev.pyw", gui_only=False)
 f()
 
 # vi:noexpandtab:sw=8:ts=8
