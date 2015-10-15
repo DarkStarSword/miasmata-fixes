4a9bc5e35e6b16832bad823540d0c711e206c0ba
Ian Munsie <darkstarsword@gmail.com>
2015-10-15 18:15:42 +1100

    Miaspatch: Add template .ts file for new translations

--- miaspatch.pro
+++ miaspatch.pro
@@ -1,2 +1,2 @@
 SOURCES = miaspatch.py miaspatch_ui.py ui_utils.py botanical.py rs5mod.py rs5archive.py
-TRANSLATIONS = miaspatch_i18n/fr_FR.ts
+TRANSLATIONS = miaspatch_i18n/miaspatch-template.ts miaspatch_i18n/fr_FR.ts
---
+++ miaspatch_i18n/miaspatch-template.ts
@@ -0,0 +1,319 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>Botanical Bad A** fix</name>
+    <message>
+        <location filename="botanical.py" line="13"/>
+        <source>Botanical Bad A** fix</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="botanical.py" line="14"/>
+        <source>Scanning Miasmata.exe...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="botanical.py" line="15"/>
+        <source>The game is already patched</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="botanical.py" line="16"/>
+        <source>Unable to locate bug</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="botanical.py" line="17"/>
+        <source>Patch successful</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="botanical.py" line="19"/>
+        <source>Error writing to Miasmata.exe</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="miaspatch_ui.py" line="277"/>
+        <source>Miasmata Patcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch_ui.py" line="278"/>
+        <source>Miasmata Install &amp;Location:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch_ui.py" line="279"/>
+        <source>&amp;Browse...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch_ui.py" line="280"/>
+        <source>The following mods will be installed:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch_ui.py" line="281"/>
+        <source>&amp;Install Mods</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch_ui.py" line="282"/>
+        <source>&amp;Remove all mods</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Errors</name>
+    <message>
+        <location filename="ui_utils.py" line="19"/>
+        <source>Unhandled Exception</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MiasPatch</name>
+    <message>
+        <location filename="miaspatch.py" line="241"/>
+        <source>Miasmata Patcher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="243"/>
+        <source>{0} does not appear to be a Miasmata install: {1} not found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="256"/>
+        <source>Loading main.rs5...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="265"/>
+        <source>Loading environment.rs5...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="375"/>
+        <source>Select Miasmata Install Location...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="420"/>
+        <source>Removing {0}...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="424"/>
+        <source>{0} occurred while removing {1}: {2}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="438"/>
+        <source>Ready</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="482"/>
+        <source>Applying main.rs5 mod order...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="525"/>
+        <source>Processing {0}...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="533"/>
+        <source>{0} occurred while copying {1}: {2}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="539"/>
+        <source>{0} occurred while processing {1}: {2}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="549"/>
+        <source>Processing installed miasmod files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="560"/>
+        <source>Loading alocalmod.rs5...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="565"/>
+        <source>alocalmod.rs5 is out of sync</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="601"/>
+        <source>{0} occurred while writing to {1}: {2}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="627"/>
+        <source>Patching game...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="634"/>
+        <source>Mods installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="640"/>
+        <source>Removing all mods...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="647"/>
+        <source>Removing environment mods...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="651"/>
+        <source>Removing main.rs5 mods...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="657"/>
+        <source>Mods removed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Mod Status</name>
+    <message>
+        <location filename="miaspatch.py" line="44"/>
+        <source>Not applicable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="45"/>
+        <source>Not installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="46"/>
+        <source>Old version installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="48"/>
+        <source>Installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="49"/>
+        <source>alocalmod.rs5 not synchronised</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="52"/>
+        <source>Version {0} installed</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PatchListModel</name>
+    <message>
+        <location filename="miaspatch.py" line="198"/>
+        <source>Mod</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="199"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="miaspatch.py" line="200"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RS5Patcher</name>
+    <message>
+        <location filename="rs5mod.py" line="80"/>
+        <source>Previously added undo metadata found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5mod.py" line="83"/>
+        <source>Undo metadata appears to be invalid, updating</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5mod.py" line="89"/>
+        <source>ERROR: {0} occured while adding undo metadata: {1}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5mod.py" line="90"/>
+        <source>REVERTING CHANGES...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5mod.py" line="93"/>
+        <source>FILE RESTORED</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5mod.py" line="373"/>
+        <source>ERROR: {0} not found in archive!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5mod.py" line="382"/>
+        <source>Removing {0}...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5mod.py" line="381"/>
+        <source>Skipping {0} - offsets do not match</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5mod.py" line="386"/>
+        <source>Rebuilding directory from mod order...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5mod.py" line="443"/>
+        <source>Skipping {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5archive.py" line="259"/>
+        <source>Adding {0}...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5archive.py" line="284"/>
+        <source>Writing RS5 header...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5archive.py" line="331"/>
+        <source>Writing central directory...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5archive.py" line="339"/>
+        <source>RS5 Written</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="rs5archive.py" line="306"/>
+        <source>Setting timestamp on %s to %s to workaround for v2.0.0.4 bug</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
