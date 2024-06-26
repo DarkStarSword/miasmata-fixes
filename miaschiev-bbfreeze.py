943ffd759eb782a0c7fd23f33a976945dbd608f4
Aura Irving <98195978+TheFriendlyRabbit@users.noreply.github.com>
2024-06-25 23:34:51 -0700

    add miaschiev requirements whoops and fix numpy path issue

--- miaschiev-bbfreeze.py
+++ miaschiev-bbfreeze.py
@@ -3,7 +3,11 @@
 from bbfreeze import Freezer
 from shutil import copyfile
 from site import getsitepackages
-from os import mkdir
+from os import mkdir, environ, pathsep
+
+# A necessary dll lives here, but this path isn't seen by default for some reason??
+environ["PATH"] += pathsep + getsitepackages()[1] + "\\numpy\\.libs"
+
 f = Freezer("miaschiev")
 f.setIcon("miasmod.ico")
 f.addScript("miaschiev.pyw", gui_only=True)
